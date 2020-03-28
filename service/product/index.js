46883aac95a5a6b267feb28b7f2fa9f337172a60
hezijian6338 <627222344@qq.com>
2020-03-28 18:58:41 +0800

    调整 product业务逻辑层代码格式

--- service/product/index.js
+++ service/product/index.js
@@ -4,12 +4,12 @@ const Uuid = require('../../utils/uuid');
 
 async function newProduct({ ctx }) {
   // 实体类字段控制
-  let new_product_info = {};
+  let newProductInfo = {};
   // 从请求体重获取信息
-  new_product_info = ctx.request.body;
+  newProductInfo = ctx.request.body;
 
   // 配置一个合法的数据库对象进行操作
-  const product = new Product(new_product_info);
+  const product = new Product(newProductInfo);
   // product.name = Reflect.get(new_product_info, "name");
   // product.brief = Reflect.get(new_product_info, "brief");
   // product.content = Reflect.get(new_product_info, "content");
@@ -26,25 +26,25 @@ async function newProduct({ ctx }) {
   // const { product_with_no_null } = product.getData();
   // console.log(product_with_no_null);
 
-  console.log(new_product_info);
+  // console.log(newProductInfo);
 
   // 执行数据库语句
   const result = await mysql('product').insert(product.getData().product);
 
-  console.log(result);
+  // console.log(result);
 
   return result[0] === 0;
 }
 
 // 获取商品信息
-async function getProduct({ ctx, id }) {
+async function getProduct({ id }) {
   // 查询 product表, 根据商品 id进行查询
-  const product_info = await mysql('product')
+  const productInfo = await mysql('product')
     .where({ product_id: id })
     .select();
 
   // 配置一个合法的对象给前端进行操作
-  const product = new Product(product_info[0]);
+  const product = new Product(productInfo[0]);
   // product.id = Reflect.get(product_info, "id");
   // product.product_id = Reflect.get(product_info, "product_id");
   // product.name = Reflect.get(product_info, "name");
@@ -60,13 +60,13 @@ async function setProduct({ ctx, id }) {
   // 先检查是否存在该商品 (方法还没想好)
 
   // 定义字段内容 (此处应该有 entity层确认, 待补充)
-  let update_product_info = {};
+  let updateProductInfo = {};
 
   // 把前端返回的数据存放到中间参数
-  update_product_info = ctx.request.body;
+  updateProductInfo = ctx.request.body;
 
   // 配置一个合法的对象给数据库进行操作
-  const product = new Product(update_product_info);
+  const product = new Product(updateProductInfo);
   // product.id = Reflect.get(update_product_info, "id");
   // product.product_id = Reflect.get(update_product_info, "product_id");
   // product.name = Reflect.get(update_product_info, "name");
@@ -86,7 +86,7 @@ async function setProduct({ ctx, id }) {
   return result === 1;
 }
 
-async function delProduct({ ctx, id }) {
+async function delProduct({ id }) {
   // 直接执行删除...
   const result = await mysql('product')
     .where({ product_id: id })
@@ -96,12 +96,13 @@ async function delProduct({ ctx, id }) {
 }
 
 // TODO: 查询所有的商品信息
-async function getProducts({ ctx }) {
+async function getProducts() {
   const result = await mysql('product').select();
+
   const products = Array;
-  for (const product of result) {
+  result.forEach((product) => {
     products.push(product);
-  }
+  });
 
   return products;
 }
