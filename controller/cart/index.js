1e39777d4daf19edca60e7dc7d61fde9f2d91404
hezijian6338 <627222344@qq.com>
2020-04-24 17:25:37 +0800

    调整了 cart和 product的代码风格 --> class方式

--- controller/cart/index.js
+++ controller/cart/index.js
@@ -1,73 +1,77 @@
 const service = require('../../service/cart')
 const Response = require('../../utils/response')
 
-async function newCart(ctx) {
-  const result = await service.newCart({ ctx })
+class CartController {
+  static async newCart(ctx) {
+    const result = await service.newCart({ ctx })
 
-  const response = new Response()
-  if (result) {
-    response.SUCCESS = 200
-  } else {
-    response.FAIL = 500
+    const response = new Response()
+    if (result) {
+      response.SUCCESS = 200
+    } else {
+      response.FAIL = 500
+    }
+
+    ctx.body = response.getData()
   }
 
-  ctx.body = response.getData()
-}
+  static async getCart(ctx) {
+    const { id } = ctx.params
 
-async function getCart(ctx) {
-  const { id } = ctx.params
+    const { cart } = await service.getCart({ ctx, id })
 
-  const { cart } = await service.getCart({ ctx, id })
-
-  const response = new Response()
-  response.SUCCESS = 200
-  response.DATA = cart
-
-  ctx.body = response.getData()
-}
-
-async function getCarts(ctx) {
-  const { userId } = ctx.params
-
-  const carts = await service.getCarts({ ctx, userId })
-
-  const response = new Response()
-  response.SUCCESS = 200
-  response.DATA = carts
-
-  ctx.body = response.getData()
-}
-
-async function setCart(ctx) {
-  const { id } = ctx.params
-
-  const result = await service.setCart({ ctx, id })
-
-  const response = new Response()
-  if (result) {
+    const response = new Response()
     response.SUCCESS = 200
-  } else {
-    response.FAIL = 500
+    response.DATA = cart
+
+    ctx.body = response.getData()
   }
 
-  ctx.body = response.getData()
-}
+  static async getCarts(ctx) {
+    const { userId } = ctx.params
 
-async function delCart(ctx) {
-  const { id } = ctx.params
+    const carts = await service.getCarts({ ctx, userId })
 
-  const result = await service.delCart({ id })
-
-  const response = new Response()
-  if (result) {
+    const response = new Response()
     response.SUCCESS = 200
-  } else {
-    response.FAIL = 500
+    response.DATA = carts
+
+    ctx.body = response.getData()
   }
 
-  ctx.body = response.getData()
+  static async setCart(ctx) {
+    const { id } = ctx.params
+
+    const result = await service.setCart({ ctx, id })
+
+    const response = new Response()
+    if (result) {
+      response.SUCCESS = 200
+    } else {
+      response.FAIL = 500
+    }
+
+    ctx.body = response.getData()
+  }
+
+  static async delCart(ctx) {
+    const { id } = ctx.params
+
+    const result = await service.delCart({ id })
+
+    const response = new Response()
+    if (result) {
+      response.SUCCESS = 200
+    } else {
+      response.FAIL = 500
+    }
+
+    ctx.body = response.getData()
+  }
 }
 
-module.exports = {
-  newCart, getCart, getCarts, setCart, delCart,
-}
+
+// module.exports = {
+//   newCart, getCart, getCarts, setCart, delCart,
+// }
+module.exports = CartController
--- controller/prodcut/index.js
+++ controller/prodcut/index.js
@@ -1,90 +1,94 @@
 const service = require('../../service/product')
 const Response = require('../../utils/response')
 
+class ProductController {
+  static async newProduct(ctx) {
+    const result = await service.newProduct({ ctx })
+
+    // 回应体抽离对象实现
+    const body = new Response()
+
+    if (result.result) {
+      // 配置成功与否和代码参数
+      body.SUCCESS = 200
+      // 配置返回内容体
+      body.DATA = result
+    } else {
+      body.FAIL = 500
+    }
+
+    // 返回信息到回应体
+    ctx.body = body.getData()
+  }
+
+  // 使用 service传递过来的信息, 返回接口信息
+  static async getProduct(ctx) {
+    const { id } = ctx.params
+
+    const { product } = await service.getProduct({ ctx, id })
+
+    // 回应体抽离对象实现
+    const body = new Response()
+    body.SUCCESS = 200
+    body.DATA = product
+
+    // 返回信息到回应体
+    ctx.body = body.getData()
+  }
+
+  static async setProduct(ctx) {
+    const { id } = ctx.params
+    const result = await service.setProduct({ ctx, id })
+
+    // 回应体抽离对象实现
+    const body = new Response()
+
+    if (result) {
+      body.SUCCESS = 200
+      body.DATA = {}
+    } else {
+      body.FAIL = 500
+    }
+
+    // 返回信息到回应体
+    ctx.body = body.getData()
+  }
+
+  static async delProduct(ctx) {
+    const { id } = ctx.params
+    const result = await service.delProduct({ ctx, id })
+
+    // 回应体抽离对象实现
+    const body = new Response()
+
+    if (result) {
+      body.SUCCESS = 200
+      body.DATA = {}
+    } else {
+      body.FAIL = 500
+    }
+
+    // 返回信息到回应体
+    ctx.body = body.getData()
+  }
+
+  static async getProducts(ctx) {
+    const products = await service.getProducts({ ctx })
+
+    // 回应体抽离对象实现
+    const body = new Response()
+    body.SUCCESS = 200
+    body.DATA = products
+
+    // 返回信息到回应体
+    ctx.body = body.getData()
+  }
+}
+
 // 路由传递进来 ctx
 
-async function newProduct(ctx) {
-  const result = await service.newProduct({ ctx })
 
-  // 回应体抽离对象实现
-  const body = new Response()
-
-  if (result.result) {
-    // 配置成功与否和代码参数
-    body.SUCCESS = 200
-    // 配置返回内容体
-    body.DATA = result
-  } else {
-    body.FAIL = 500
-  }
-
-  // 返回信息到回应体
-  ctx.body = body.getData()
-}
-
-// 使用 service传递过来的信息, 返回接口信息
-async function getProduct(ctx) {
-  const { id } = ctx.params
-
-  const { product } = await service.getProduct({ ctx, id })
-
-  // 回应体抽离对象实现
-  const body = new Response()
-  body.SUCCESS = 200
-  body.DATA = product
-
-  // 返回信息到回应体
-  ctx.body = body.getData()
-}
-
-async function setProduct(ctx) {
-  const { id } = ctx.params
-  const result = await service.setProduct({ ctx, id })
-
-  // 回应体抽离对象实现
-  const body = new Response()
-
-  if (result) {
-    body.SUCCESS = 200
-    body.DATA = {}
-  } else {
-    body.FAIL = 500
-  }
-
-  // 返回信息到回应体
-  ctx.body = body.getData()
-}
-
-async function delProduct(ctx) {
-  const { id } = ctx.params
-  const result = await service.delProduct({ ctx, id })
-
-  // 回应体抽离对象实现
-  const body = new Response()
-
-  if (result) {
-    body.SUCCESS = 200
-    body.DATA = {}
-  } else {
-    body.FAIL = 500
-  }
-
-  // 返回信息到回应体
-  ctx.body = body.getData()
-}
-
-async function getProducts(ctx) {
-  const products = await service.getProducts({ ctx })
-
-  // 回应体抽离对象实现
-  const body = new Response()
-  body.SUCCESS = 200
-  body.DATA = products
-
-  // 返回信息到回应体
-  ctx.body = body.getData()
-}
-
-module.exports = {
-  newProduct, getProduct, setProduct, delProduct, getProducts,
-}
+// module.exports = {
+//   newProduct, getProduct, setProduct, delProduct, getProducts,
+// }
+module.exports = ProductController
