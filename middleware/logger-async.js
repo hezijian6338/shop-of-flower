95ff9f8f7d1243c0b7bc16cded33703c328ca24b
hezijian6338 <627222344@qq.com>
2020-05-27 08:48:39 +0800

    再添加一种写法, 提供两种思路进行处理鉴权路径没携带 Token

--- middleware/logger-async.js
+++ middleware/logger-async.js
@@ -13,6 +13,7 @@ module.exports = function () {
     log(ctx)
     // console.log(ctx.url)
     if (!ctx.url.includes('/login') && !ctx.url.includes('/tag') && !ctx.url.includes('/product') && !ctx.url.includes('/sku')) {
+      // TODO: 写法一, 判定字段是否为空 (需要对错误发生点比较熟悉)
       const { authorization } = ctx.header
       if (authorization === null || authorization === undefined) {
         ctx.body = { message: '授权错误' }
@@ -21,6 +22,17 @@ module.exports = function () {
         ctx.body = { phone: username }
         await next()
       }
+
+      // TODO: 写法二, 直接 try/catch进行捕获异常, 这样不用过分关注可能发生的错误问题点在哪
+      // const { authorization } = ctx.header
+      // try {
+      //   const username = await verify(authorization.split(',')[0].split(' ')[1], jwtSecret)
+      //   ctx.body = { phone: username }
+      //   await next()
+      // } catch (e) {
+      //   console.log(e)
+      //   ctx.body = { message: '授权错误' }
+      // }
     } else {
       log(ctx)
       await next()
